9e67b93a6cfcf899efbc113f5e9bcded127ca8ca
Russell Azucenas <45620071+Ninjaruss@users.noreply.github.com>
2019-11-07 18:46:35 -0800

    Prototyping App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+const [local_id, inputId] = React.useState('');
+const [local_body, inputBody] = React.useState('');
+const url = 'http://localhost:1234'
+//const [list, setList] = React.useState('');
+
+const saveNote = () => {
+  const requestObj = {
+    _id: local_id,
+    body: local_body,
+  }
+
+  if (local_body !== ''){
+    if (local_id !== ''){
+      // Update a note
+      axios.post(url.concat('/update'), requestObj)
+        .then(console.log)
+        .catch(console.log)
+    } else {
+      // Store new note
+      axios.post(url.concat('/store'), requestObj)
+        .then(console.log)
+        .catch(console.log)
+    }
+  }
+};
+
+const deleteNote = () => {
+  if (local_id !== ''){
+    axios.post(url.concat('/delete'), {_id: local_id})
+      .then(console.log)
+      .catch(console.log)
+  }
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>_id</h1>
+        <input value={local_id} onChange={e => inputId(e.target.value)} />
+        <h1>body</h1>
+        <input value={local_body} onChange={e => inputBody(e.target.value)} />
+        <button onClick={saveNote}>Save</button>
+        <button onClick={deleteNote}>Delete</button>
       </header>
     </div>
   );
